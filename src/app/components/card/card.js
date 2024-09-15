caadb087ae4018f5d27889a96bf5149cb425cc56
Álvaro Sales Balbino <97653093+Alvaro-Sales@users.noreply.github.com>
2024-09-15 01:18:23 -0300

    Add files via upload
    Correção da responsividade para mobile

--- src/app/components/card/card.js
+++ src/app/components/card/card.js
@@ -5,8 +5,8 @@ import Link from "next/link"
 export default function Card({imagem, title, description, link}){
     return(
         <>
-            <div className="cardAnime w-[400px] h-[580px] bg-[var(--color-three)] flex flex-col items-center justify-between rounded-3xl">
-                {imagem && <div className="mb-5 h-[50%]"> {imagem} </div>}
+            <div className="cardAnime card w-[400px] h-[580px] bg-[var(--color-three)] flex flex-col items-center justify-between rounded-3xl">
+                {imagem && <div className="mb-5 h-[50%] w-full"> {imagem} </div>}
 
                 <div className="flex flex-col items-center justify-center p-5 box-border h-[50%]">
                     {title && <h1 className="text-3xl mb-5"> {title} </h1>}
--- src/app/page.js
+++ src/app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
 
 						<p className="text-base text-justify mb-10 textAnimate">Conheça os nossos serviços e projetos que com certeza irão te deixar sempre à frente da concorrência! Aqui a sua identidade visual é prioridade e vamos transformala para ter muito mais personalidade e potencial.</p>
 
-						<div className="flex items-center justify-center gap-5 flex-wrap mobile:gap-3 mobile:flex-col">
-							<button className="bg-[var(--color-three)] rounded-xl hover:bg-[var(--color-two)] hover:text-[var(--text-color)] transition-all duration-300 btnAnimate">
+						<div className="w-full flex items-center justify-center gap-5 flex-wrap mobile:flex-col">
+							<button className="w-[280px] bg-[var(--color-three)] rounded-xl hover:bg-[var(--color-two)] hover:text-[var(--text-color)] transition-all duration-300 btnAnimate">
 								{/* link que direciona para o Whatsapp http://wa.me/5582993263814?text=Oii%20MetaBusiness%20 */}
 								<Link href="#" className="flex items-center justify-center px-5 py-2 bg-transparent gap-5 rounded-xl desktop2:px-3">
 									<span className="text-xl tracking-wider uppercase">Sair na frente</span>
@@ -23,7 +23,7 @@ export default function Home() {
 								</Link>
 							</button>
 							
-							<button className="bg-[var(--color-three)] rounded-xl hover:bg-[var(--color-two)] hover:text-[var(--text-color)] transition-all duration-300 btnAnimate">
+							<button className="w-[280px] bg-[var(--color-three)] rounded-xl hover:bg-[var(--color-two)] hover:text-[var(--text-color)] transition-all duration-300 btnAnimate">
 								<Link href="./projetos" className="flex items-center justify-center px-5 py-2 bg-transparent gap-5 rounded-xl desktop2:px-3">
 									<span className="text-xl tracking-wider uppercase">Ver Projetos</span>
 									<MdOutlineImportantDevices className="text-4xl" />
@@ -32,7 +32,7 @@ export default function Home() {
 						</div>
 					</div>
 
-					<div className="flex items-center justify-center max-w-[50%] max-h-full mobile:h-[326px] mobile:w-[400px]">
+					<div className="flex items-center justify-center max-w-[50%] max-h-full mobile:h-[326px] mobile:w-[400px] mobile:max-w-full">
 						<Image
 							src="/images/notebook-logo.png"
 							alt="Notebook com o logotipo da agência Meta Business na tela"

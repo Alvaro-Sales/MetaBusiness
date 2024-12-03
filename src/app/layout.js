641aa24df9863989917bf11dbfe724ae3669fa28
Álvaro Sales Balbino <97653093+Alvaro-Sales@users.noreply.github.com>
2024-12-03 08:17:53 -0300

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,21 @@
-import "./globals.css"
-import Footer from "./components/footer/footer"
+import NavbarFloat from "../components/NavbarFloat/NavbarFloat.js"
 import { Inter } from "next/font/google"
-import Header from "./components/header/header"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  	title: "Meta Business Corp",
-  	description: "Agência de gestão comercial para melhoria de identidade visual e melhor visibilidade comercial.",
+  title: "Portfólio | Álvaro Sales",
+  description: "Portfólio pessoal de um dev frontend",
 }
 
 export default function RootLayout({ children }) {
-	return (
-    	<html lang="pt-br">
-      		<body className={inter.className}>
-				<Header />
-        		
-				<main className="w-full max-w-[100vw] overflow-x-hidden">
-					{children}
-				</main>
-
-				<Footer />
-      		</body>
-    	</html>
-  	)
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <NavbarFloat />
+        {children}
+      </body>
+    </html>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      'desktop': {'min': '1256px'},
-      'desktop2': {'max': '1257px'},
-      'laptop': {'max': '1200px'},
-      'tabletG': {'max': '1040px'},
-      'mobile': {'max': '850px'},
+    extend: {
+      screens: {
+        'laptop': {'max': '1080px'},
+        'tablet': {'max': '950px'},
+        'mobile': {'max': '650px'},
+      },
     },
   },
   plugins: [],
